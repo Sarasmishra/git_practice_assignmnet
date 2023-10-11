07a69dadf569b82bb77b5d2c9ed9a1d66b563853
Sarasmishra <sarasmishra8100@gmail.com>
2023-10-11 11:55:09 +0530

    added a primenumber file

---
+++ primenumber.js
@@ -0,0 +1,21 @@
+let number =4;
+let flag = 0;
+
+    for(let i=2;i<=number;i++){
+       if(number%i==0){
+            flag++;
+            
+       }
+
+    }
+if(flag<=4){
+    console.log("prime number")
+}
+else{
+    console.log("Not a prime number")
+}
+
+
+
+
+
